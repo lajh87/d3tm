75c273aa514eabffb0ba4a52142092ce093c3d1e
lajh87 <lajh87@me.com>
2018-12-22 15:25:33 +0000

    Update of zoomableTreemap.js to include event click shiny app to demonstrate
    Minor Updates
    * Update to meta data in description
    * include flare.json into inst folder (download from [d3 github
    repo](https://raw.githubusercontent.com/d3/d3-hierarchy/master/test/data/flare.json))

---
+++ inst/shiny/example-1/app.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(d3RZoomableTreemap)
+
+# Using flare as have different levels and removing any json modifications
+flare <- jsonlite::fromJSON(
+  txt = system.file("json/flare.json",package = "d3RZoomableTreemap"),
+  flatten = F, simplifyVector = F, simplifyDataFrame = F, simplifyMatrix = F
+  )
+
+ui <- fluidPage(
+  sidebarLayout(
+    mainPanel = mainPanel(zoomableTreemapOutput("x1")),
+    sidebarPanel = sidebarPanel(
+      fluidRow(
+        tags$label("Selected Node:"),textOutput("event",inline = T)
+        )
+      ),
+      position = "right"
+    )
+  )
+
+server <- function(input, output, session) {
+
+  output$x1 <- renderZoomableTreemap({
+    d3RZoomableTreemap::zoomableTreemap(flare)
+  })
+
+  output$selected_node <- renderText({input$x1_click})
+
+  }
+
+shinyApp(ui, server)
