8eb3a52af80842d9c999728f5481e633a3efc156
Luke Heley <lajh87@me.com>
2019-01-13 13:42:27 +0000

    updated d3 nest test

--- tests/testthat/test_d3_nest2.R
+++ tests/testthat/test_d3_nest2.R
@@ -2,8 +2,7 @@ context("d3_nest")
 test_that("Check output is json",{
   input <- data.frame(Titanic) %>%
     dplyr::select(Class,Age,Survived,Sex,Freq) %>%
-    d3_nest2(value_col="Freq", root="Titanic",
-             id_vars = c("Class", "Age", "Survived", "Sex"))
+    d3_nest2(value_col="Freq", root="Titanic")
   expect_is(input, "json")
 })
 
