67bc086a1ab03a246ff42ec6309e6fbfc99084dd
Tom Westlake <tom.westlake121@gmail.com>
2019-04-13 18:46:34 +0100

    fix devtools check

--- tests/testthat/test_d3_nest2.R
+++
@@ -1,9 +0,0 @@
-context("d3_nest")
-test_that("Check output is json",{
-  input <- data.frame(Titanic) %>%
-    dplyr::select(Class,Age,Survived,Sex,Freq) %>%
-    d3_nest2(value_col="Freq", root="Titanic")
-  expect_is(input, "json")
-})
-
-
