c15c0c39f9ce86907eace1aac285458ce60aad39
Luke Heley <lajh87@me.com>
2021-06-06 14:26:53 +0100

    fix issue with df_to_tree where level 1 with no children causes fail

--- R/df_to_tree.R
+++ R/df_to_tree.R
@@ -13,7 +13,8 @@
 #' @export
 #'
 #' @examples
-#' data.frame(path = c("1/A", "1/B/i", "1/B/ii", "2A"), value = 1:4)
+#' library(d3tm)
+#' df <- data.frame(path = c("1/A", "1/B/i", "1/B/ii", "2/A", 3), value = 1:5)
 #' df_to_tree(df)
 df_to_tree <- function(df, stringPath = "path", stringSep = "/",
                        value_from = "value", root_name = "root"){
@@ -23,26 +24,32 @@ df_to_tree <- function(df, stringPath = "path", stringSep = "/",
 
   filter_children <- function(l, j = 1, parent) {
 
+    ## Extract a unique vector of children names for a given level and parent
     children <- do.call(rbind, purrr::map(l, function(x){
       if(j == 1) x[j] else
-        if(x[j-1] == parent) x[j]
+        if(!is.na(x[j-1]) && x[j-1] == parent)  x[j]
     })) %>% as.character() %>% unique()
 
+    ## Extract the leaf values and associated child's name for a given parent
     values <- do.call(rbind, purrr::map(1:length(l), function(i){
 
-      if(j == 1){
-        data.frame(name = l[[i]][j],
-                   value = value[i],
-                   stringsAsFactors = FALSE)
-      }
-
-      if(j>1 && l[[i]][j-1] == parent)
-        if(length(l[[i]]) == j)
+      if(length(l[[i]]) == j){
+        if(j==1){
           data.frame(name = l[[i]][j],
                      value = value[i],
                      stringsAsFactors = FALSE)
+        } else{
+          if(l[[i]][j-1] == parent){
+            data.frame(name = l[[i]][j],
+                       value = value[i],
+                       stringsAsFactors = FALSE)
+          }
+        }
+      }
     }))
 
+    ## Loop through children and for each child if they have children then
+    ##  extract children. If not the extract the leaf values.
     purrr::map(children, function(child){
       if(has_children(l, j, child)){
         list(name = child, children = filter_children(l, j+1, child))
@@ -51,6 +58,7 @@ df_to_tree <- function(df, stringPath = "path", stringSep = "/",
     })
   }
 
+  ## Check if a given child has children of their own.
   has_children <- function(l, level, child){
     children <- purrr::map(l, function(x){
       if(!is.na(x[level]))
